c3941d621e1347a9c37ba66179f64197748e913c
sanjayram-07 <m.sanjayram07@gmail.com>
2025-07-31 22:04:12 +0530

    Update doulbylinked_list.cpp

--- doulbylinked_list.cpp
+++ doulbylinked_list.cpp
@@ -20,39 +20,70 @@ class dll{
 
     public:
         node* head = nullptr;
-        node* temp = nullptr;
-        node* newnode = nullptr;
+        node* tail = nullptr;
+       
 
-        void push_back(int val)
+        void create(int val)
         {
-            newnode =  new node(val);
+            node* newnode =  new node(val);
             if(head == nullptr)
             {
-                head = temp = newnode;
+                head = tail = newnode;
             }
             else
             {
-                temp->prev = temp;
-                temp->next= newnode;
-                
-                temp = temp->next;
+                tail->next= newnode;
+                newnode->prev = tail;
+                tail = newnode;
             }
         }
+        void insert_begin(int val)
+        {
+            node * newnode = new node(val);
+            head->prev = newnode;
+            newnode->next = head;
+            head = newnode;
+        }
+        void insert_back(int val)
+        {
+            node * newnode = new node(val);    
+            tail->next = newnode;
+            newnode->prev = tail;
+            tail = newnode;
+        }
+        void delete_begin()
+        {
+            node * temp = head;
+            head = head->next;
+            head->prev = nullptr;
+            delete temp;
+        }      
+        void delete_end()
+        {
+            node * temp = tail ;
+            tail= tail->prev;
+            tail->next = nullptr;
+            delete temp;
+        }
+    
         void display()
         {
-            node * temp1 =head;
-            while(temp1!=nullptr)
+            node * temp =head;
+            while(temp!=nullptr)
             {
-                cout<<temp1->data<<" ";
-                temp1 = temp1->next;
+                cout<<temp->data<<" ";
+                temp = temp->next;
             }
         }
 };
 int main()
 {
     dll s;
-    s.push_back(67);
-    s.push_back(65);
-    s.push_back(44);
+    s.create(67);
+    s.insert_begin(78);
+    s.insert_back(44);
+    s.insert_back(76);
+    s.delete_begin();
+    s.delete_end();
     s.display();
 }
