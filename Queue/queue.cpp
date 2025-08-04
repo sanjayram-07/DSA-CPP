ae5b6243d2e3d011b1fffae7b56c2fd7b6b1b3db
sanjayram-07 <m.sanjayram07@gmail.com>
2025-08-04 14:39:35 +0530

    Update queue.cpp

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -1,61 +1,117 @@
 #include<iostream>
 using namespace std;
-class queue{
-    int arr[100];
-    int front ;
-    int rear;
-
-    public:
-    queue()
-    {
-        front = rear =-1;
-    }
-
-    void enqueue(int val)
-    {
-        if(rear == 100)
+class queue
+{
+ int arr[10];
+ int front;
+ int rear;
+ int max_size = 10;
+  public:
+ queue()
+{
+        front = rear=-1;
+}
+void read()
         {
-            cout<<"max_size reached";
+                int a;
+                cout<<"Enter the number of elements less than 10: ";
+                cin>>a;
+                front =0;
+                for(int i =front ; i < a; i++)
+                {
+                        cout<<"Enter the element value  "<<i+1<<": ";
+                        cin>>arr[i];
+                }
+                rear = a-1;
+        }
+
+
+  void enqueue(int val)
+  {
+          if(rear == max_size-1)
+          {
+                  cout<<"The queue is full "<<endl;
+                  return;
+          }
+          if(front ==-1)
+        {
+                front =0;
+                arr[front]= val;
+                rear++;
+
+        }
+          else
+          {
+                  rear = rear+1;
+                  arr[rear] = val;
+
+          }
+
+  }
+void dequeue()
+{
+
+        if(front==rear && front !=-1)
+        {
+                cout<<"The dequeued element is: "<<arr[front]<<endl;
+                front = rear =-1;
+
         }
         if(front ==-1)
         {
-            front =0;
-            arr[front] = val;
-            rear++;
+                cout<<"The queue is EMPTY "<<endl;
         }
-        else{
-            arr[++rear]= val;
-        }
-
-
-    }
-    void dequeue()
-    {
-        if(front == rear)
+        else
         {
-            cout<<"the element is "<<arr[front]<<endl;
-            front = rear =-1;
+                cout<<"The dequeued element is : "<<arr[front]<<endl;
+                front++;
         }
-        else{
-        cout<<"the element is"<<arr[front]<<endl;
-        front++;
-        }
-    }
-    void display()
-    {
-        for(int i=front ; i<= rear ; i++)
-        {
-            cout<<arr[i]<<" ";
-        }
-    }
-
-};
-int main()
-{
-    queue q;
-    q.enqueue(5);
-    q.enqueue(13);
-    q.dequeue();
-    q.display();
 
 }
+void display()
+{
+        if(front ==-1)
+        {
+                cout<<"The queue is empty nothing to Print "<<endl;
+        }
+        else{
+        for(int i =front ;i <= rear;i++)
+        {
+                cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        }
+}
+};
+int main(){
+        queue q;
+        q.read();
+        int x;
+        do{
+                cout<<"1.enqueue"<<endl;
+                cout<<"2.dequeue"<<endl;
+                cout<<"3.display"<<endl;
+                cout<<"0.exit"<<endl;
+                cout<<"Enter your choice: ";
+                cin>>x;
+                switch(x)
+                {
+                        case 1:
+                                int value;
+                                cout<<"Enter the value need to be enqueued :";
+                                cin>>value;
+                                q.enqueue(value);
+                                break;
+                        case 2:
+                                q.dequeue();
+                                break;
+                        case 3:
+                                q.display();
+                                break;
+                        default:
+                                break;
+                }
+
+                }while(x !=0);
+
+        }
